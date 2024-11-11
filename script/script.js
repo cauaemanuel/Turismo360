2c5bcb7cc9bbebecf568699f3c76c3afdd10db6d
caua <emanullcaua8@gmail.com>
2024-11-11 01:52:00 -0300

    Update Menu

--- script/script.js
+++ script/script.js
@@ -1,9 +1,11 @@
 function toggleMenu() {
     const menu = document.getElementById("menu");
     const menuIcon = document.getElementById("menuIcon");
+    const hamburguer = document.querySelector(".hamburguer");
 
     // Alterna a classe `open` no menu para abri-lo ou fechá-lo
     menu.classList.toggle("open");
+    hamburguer.classList.toggle("fixed");
 
    
     if (menu.classList.contains("open")) {
