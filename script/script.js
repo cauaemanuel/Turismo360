4aa62c7f9f5e3d9fa4c06b3b7e5bf99c9d4da937
caua <emanullcaua8@gmail.com>
2024-12-10 12:33:56 -0300

    map correct

--- script/script.js
+++ script/script.js
@@ -38,3 +38,22 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 window.addEventListener("resize", ajustarMenu); // Ajusta o menu ao redimensionar
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const cidades = document.querySelectorAll(".lista-cidades li");
+    const mapFrame = document.getElementById("mapFrame");
+
+    cidades.forEach(cidade => {
+        cidade.addEventListener("click", () => {
+            const src = cidade.getAttribute("data-src"); // Obtém o atributo data-src
+            mapFrame.src = src; // Atualiza o src do iframe
+        });
+    });
+});
+
+
+//   recife         <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d75163.17291162205!2d-34.92690468695944!3d-8.073136899063146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1spontos%20turisticos%20recife!5e0!3m2!1sen!2sbr!4v1733843792761!5m2!1sen!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>        </div>
+// paulista    <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d44706.83675414563!2d-34.86880126641423!3d-7.9405790294033896!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1spontos%20tur%C3%ADsticos%20near%20Paulista%20-%20State%20of%20Pernambuco!5e0!3m2!1sen!2sbr!4v1733843989650!5m2!1sen!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+// olinda <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d22349.981448711875!2d-34.86055953211083!3d-8.003489176175774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1spontos%20tur%C3%ADsticos%20near%20Olinda%2C%20State%20of%20Pernambuco!5e0!3m2!1sen!2sbr!4v1733844086115!5m2!1sen!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+// jaboatao <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d252798.99871782525!2d-35.06166771476362!3d-8.103074534179779!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1spontos%20tur%C3%ADsticos%20near%20Jaboat%C3%A3o%20dos%20Guararapes%20-%20Rua%20Vinte%20e%20Um%20-%20Curado%20I%2C%20Jaboat%C3%A3o%20dos%20Guararapes%20-%20State%20of%20Pernambuco!5e0!3m2!1sen!2sbr!4v1733844190494!5m2!1sen!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
